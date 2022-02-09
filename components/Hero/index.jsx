757ce72b37f8948ad70e14227f8e3fee30c4d083
dior <diorrego1@gmail.com>
2022-02-09 01:03:12 -0300

    :sparkles: add Hero components

---
+++ components/Hero/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section>
+      <video
+        autoPlay
+        loop
+        muted
+        className="object-cover overflow-hidden h-screen sm:h-full sm:w-full"
+      >
+        <source src="/video.mp4" type="video/mp4" />
+      </video>
+    </section>
+  );
+};
+
+export default Hero;
