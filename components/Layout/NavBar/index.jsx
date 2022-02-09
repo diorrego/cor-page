4dc4d4308a77586ddd83ac23e92d8798bd5aa4c2
dior <diorrego1@gmail.com>
2022-02-09 01:04:39 -0300

    :lipstick: change background color

--- components/Layout/NavBar/index.jsx
+++ components/Layout/NavBar/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <nav className="absolute inset-x-0 top-0 flex flex-row justify-around w-full bg-green-600 z-50 py-4">
-      <button className="rounded-full bg-yellow-200 text-gray-500 w-10 h-10">
+    <nav className="absolute inset-x-0 top-0 flex flex-row justify-around w-full bg-sky-400 bg-opacity-40 text-white z-50 py-4">
+      <button className="rounded-full bg-white text-gray-500 w-10 h-10">
         cor
       </button>
-      <div className="flex flex-row item-rigth space-x-6">
+      <div className="flex flex-row item-rigth space-x-16">
         <button>Botellas</button>
         <button>Un regalo</button>
       </div>
