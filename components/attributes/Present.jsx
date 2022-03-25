e8221b762cf5d583e60fd892a1720781edf3b198
dior <diorrego1@gmail.com>
2022-03-25 16:09:03 -0300

    :lipstick: change max with

--- components/attributes/Present.jsx
+++ components/attributes/Present.jsx
@@ -9,7 +9,7 @@ const Present = () => {
   return (
     <section className="relative overflow-y-hidden sm:h-96">
       <Image src={cordillera} alt="Nevados de Chillán" layout="responsive" />
-      <div className="absolute flex flex-col justify-center items-start left-8 top-8 py-1 sm:py-0 sm:top-3.5 xl:inset-20 xl:left-96 space-y-2 max-w-xs sm:max-w-md bg-white-cor/60 px-4 rounded-lg">
+      <div className="absolute flex flex-col justify-center items-start left-8 top-8 py-1 sm:py-0 sm:top-3.5 xl:inset-20 xl:left-96 space-y-2 max-w-[17rem] sm:max-w-md bg-white-cor/60 px-4 rounded-lg">
         <h1 className="title">Un regalo de la naturaleza</h1>
         <p className="description hidden sm:block">
           <CorLogo className="inline" /> es extraída y producida en un paraje
