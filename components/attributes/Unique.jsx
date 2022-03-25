7fcab976c24d305631c6900e8016c190f012840e
dior <diorrego1@gmail.com>
2022-03-25 16:29:03 -0300

    :iphone: fix responsive

--- components/attributes/Unique.jsx
+++ components/attributes/Unique.jsx
@@ -6,9 +6,13 @@ import botellaVidrio from '../../public/botellaVidrio.jpeg';
 
 const Unique = () => {
   return (
-    <section className="flex flex-col space-y-12 px-8 py-4 sm:px-96 sm:py-20">
-      <h1 className="title">Única para tu estilo</h1>
+    <section className="flex flex-col space-y-4 xl:space-y-12 px-8 py-4 sm:py-20">
       <div className="sm:flex sm:flex-row sm:justify-around">
+        <h1 className="title">Única para tu estilo</h1>
+        <div></div>
+      </div>
+      <div></div>
+      <div className="sm:flex sm:flex-row sm:justify-evenly">
         <ProductSimpleView
           slogan="Única y exclusiva"
           image={botellaVidrio}
