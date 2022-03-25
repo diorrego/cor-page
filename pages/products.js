c4f8b8039d35995b15b712c013876f887b306859
dior <diorrego1@gmail.com>
2022-03-25 14:56:25 -0300

    :art: simplify structure

--- pages/products.js
+++ pages/products.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import Layout from '../components/Layout';
 import DoubleWave from '../components/waves/DoubleWave';
-import FullView from '../components/products/FullView';
+import ProductFullView from '../components/products/ProductFullView';
 
 import botellaVidrio from '../public/botellaVidrio.jpeg';
 
@@ -21,19 +20,20 @@ export default function Products() {
       <Head>
         <title>cor Chile | Products</title>
       </Head>
-      <section className="pt-40 flex flex-col w-full">
-        <FullView
+      <section className="pt-24 sm:pt-32 flex flex-col w-full">
+        <ProductFullView
           image={botellaVidrio}
-          title="Única y exclusiva"
+          slogan="Única y exclusiva"
           name="Cordillera"
           description="Arte y comodidad para recibir la energía de la cordillera."
         />
         <DoubleWave />
-        <FullView
+        <ProductFullView
           image={botellaVidrio}
-          title="Única y divertida"
+          slogan="Única y divertida"
           name="Viento"
           description="Todo el poder de la cordillera para los amantes del movimiento y la salud."
+          className="bg-container pb-24 sm:pb-32 text-white-cor"
         />
       </section>
     </Layout>
