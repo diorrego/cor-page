964c13e4ee0d8ccb8d49125367d794f74266b7dc
dior <diorrego1@gmail.com>
2022-03-25 14:21:03 -0300

    :sparkles: add cor logo component

--- components/CorLogo/index.jsx
+++ components/CorLogo/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CorLogo = ({ className }) => {
-  return <h1 className={`font-cor text-3xl ${className}`}>cor</h1>;
+  return <h1 className={`font-cor text-3xl p-auto ${className}`}>cor</h1>;
 };
 
 export default CorLogo;
