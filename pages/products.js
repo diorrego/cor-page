ff825bbeda80b3ec9994250fd5f67ebce4b3edab
dior <diorrego1@gmail.com>
2022-03-25 13:12:07 -0300

    :recycle: change products to component

--- pages/products.js
+++ pages/products.js
@@ -5,6 +5,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import Layout from '../components/Layout';
 import DoubleWave from '../components/waves/DoubleWave';
+import FullView from '../components/products/FullView';
 
 import botellaVidrio from '../public/botellaVidrio.jpeg';
 
@@ -21,38 +22,19 @@ export default function Products() {
         <title>cor Chile | Products</title>
       </Head>
       <section className="pt-40 flex flex-col w-full">
-        <div className="flex flex-col space-y-4 px-96 ">
-          <h1 className="title mb-10">
-            Única <span className="font-semibold">y exclusiva</span>
-          </h1>
-          <h2 className="subtitle">Cordillera</h2>
-          <p className="description">
-            Estilo, comodidad y arte
-            <span className="font-medium">
-              para recibir la energía de la cordillera.
-            </span>
-            .
-          </p>
-          <div className="flex flex-row items-center justify-around">
-            <Image src={botellaVidrio} alt="Botella Cordillera" />
-            <div>Info nutricional, peso, diseño botella, etc.</div>
-          </div>
-        </div>
+        <FullView
+          image={botellaVidrio}
+          title="Única y exclusiva"
+          name="Cordillera"
+          description="Arte y comodidad para recibir la energía de la cordillera."
+        />
         <DoubleWave />
-        <div className="flex flex-col space-y-4 px-96 bg-[#0693e3ff] pb-60">
-          <h1 className="title mb-10">
-            Única <span className="font-semibold">y divertida</span>
-          </h1>
-          <h2 className="text-xl font-semibold">Viento</h2>
-          <p className="description">
-            Para los amantes del movimiento{' '}
-            <span className="font-medium">y la salud.</span>
-          </p>
-          <div className="flex flex-row items-center justify-around">
-            <Image src={botellaVidrio} alt="Botella Cordillera" />
-            <div>Info nutricional, peso, diseño botella, etc.</div>
-          </div>
-        </div>
+        <FullView
+          image={botellaVidrio}
+          title="Única y divertida"
+          name="Viento"
+          description="Todo el poder de la cordillera para los amantes del movimiento y la salud."
+        />
       </section>
     </Layout>
   );
