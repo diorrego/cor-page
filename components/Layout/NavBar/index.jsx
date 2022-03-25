a8bec0a4cd9a2f775176980de4b47388015af8f9
dior <diorrego1@gmail.com>
2022-03-25 13:27:50 -0300

    :lipstick: change colors and migrate cor logo

--- components/Layout/NavBar/index.jsx
+++ components/Layout/NavBar/index.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
+import CorLogo from '../../CorLogo';
+
 const NavBar = () => {
   return (
-    <nav className="fixed inset-x-0 top-0 flex flex-row justify-around px-2 sm:justify-around items-center w-screen bg-sky-400 bg-opacity-40 text-white z-50 py-4">
+    <nav className="fixed inset-x-0 top-0 flex flex-row justify-around px-2 sm:justify-around items-center w-screen bg-surface bg-opacity-60 text-white-cor z-50 py-4">
       <Link href="/" passHref>
-        <button className="rounded-full bg-white text-gray-500 w-10 h-10">
-          cor
-        </button>
+        <motion.button>
+          <CorLogo />
+        </motion.button>
       </Link>
-
       <div className="flex flex-row item-rigth space-x-5 sm:space-x-16">
         <Link href="/products" passHref>
           <motion.button>Productos</motion.button>
