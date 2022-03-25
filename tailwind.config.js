77c962bad6c268dc37f7d842cf6fea037e3f06a1
dior <diorrego1@gmail.com>
2022-03-25 12:49:13 -0300

    :lipstick: add colors and typography stow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#FFFCE6',
+        surface: '#0B2740',
+        container: '#1C66A6',
+        input: '#4C5938',
+        primary: '#6BB3F2',
+        'primary-variant': '#80C7F2',
+        accent: '#D9CB04',
+        secondary: '#A8BF54',
+        'secondary-variant': '#CFD984',
+        'white-cor': '#FEEFE7',
+      },
+    },
+    fontFamily: {
+      sans: ['"Raleway"', '"Signika"', ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 };
