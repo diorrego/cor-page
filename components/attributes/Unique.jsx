09f9aea0f12677af31e537f2729ac9b62a001e75
dior <diorrego1@gmail.com>
2022-03-26 01:15:17 -0300

    :lipstick: change padding

--- components/attributes/Unique.jsx
+++ components/attributes/Unique.jsx
@@ -6,7 +6,7 @@ import botellaVidrio from '../../public/botellaVidrio.jpeg';
 
 const Unique = () => {
   return (
-    <section className="flex flex-col space-y-4 xl:space-y-12 px-8 py-4 sm:py-20">
+    <section className="flex flex-col space-y-4 xl:space-y-12 px-8 py-4 sm:py-36">
       <div className="sm:flex sm:flex-row sm:justify-around">
         <h1 className="title">Única para tu estilo</h1>
         <div></div>
