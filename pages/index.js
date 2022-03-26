7be648ce035b8dd07065cbba69ef013440542546
dior <diorrego1@gmail.com>
2022-03-26 01:16:18 -0300

    :sparkles: add bottle

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 
@@ -7,6 +8,8 @@ import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 import Present from '../components/attributes/Present';
 import Unique from '../components/attributes/Unique';
+
+import botella from '../public/botella.png';
 
 export const getStaticProps = async ({ locale }) => {
   return {
@@ -22,6 +25,10 @@ export default function Home() {
       <Head>
         <title>cor Chile</title>
       </Head>
+      <div className="absolute top-32 md:top-40 lg:top-44 xl:top-50 xl:left-28 w-5/12">
+        <Image src={botella} alt="Cordillera" layout="responsive" />
+        <div></div>
+      </div>
       <Hero />
       <Unique />
       <Present />
