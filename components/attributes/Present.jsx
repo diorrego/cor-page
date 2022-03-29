9af2900b28be1570bfda68a012fe0cdfdec3cc16
dior <diorrego1@gmail.com>
2022-03-28 23:03:58 -0300

    :art: remove CorLogo Component

--- components/attributes/Present.jsx
+++ components/attributes/Present.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
 
-import CorLogo from '../CorLogo';
-
 import cordillera from '../../public/cordillera.jpg';
 
 const Present = () => {
@@ -12,9 +10,8 @@ const Present = () => {
       <div className="absolute flex flex-col justify-center items-start left-8 top-8 py-1 px-4 sm:py-0 sm:top-3.5 xl:inset-20 xl:left-96 space-y-2 max-w-[17rem] sm:max-w-md bg-white-cor/60 rounded-lg">
         <h1 className="title">Un regalo de la naturaleza</h1>
         <p className="description hidden sm:block">
-          <CorLogo className="inline" /> es extraída y producida en un paraje
-          virgen e inmaculado posado en la falda de la joven Cordillera de los
-          Andes.
+          Cor es extraída y producida en un paraje virgen e inmaculado posado en
+          la falda de la joven Cordillera de los Andes.
         </p>
         <h2 className="font-semibold">Atacalco | Región del Ñuble | Chile</h2>
       </div>
