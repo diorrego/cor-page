7b1b720fffeaab1e821513254f6f95285d54645d
dior <diorrego1@gmail.com>
2022-03-29 02:35:51 -0300

    :recycle: add bottle

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Image from 'next/image';
 
 import useBreakpoint from '../../hooks/useBreakpoint';
+
+import botella from '../../public/botella.png';
 
 const Hero = () => {
   const isMobile = !useBreakpoint('sm');
@@ -8,15 +11,18 @@ const Hero = () => {
   return isMobile ? (
     <section className="bg-background h-screen w-full"></section>
   ) : (
-    <section>
+    <section className="z-10">
       <video
         autoPlay
         loop
         muted
         className="object-cover overflow-hidden max-h-[54rem] w-full"
       >
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="video.mp4" type="video/mp4" />
       </video>
+      <div className="hidden sm:block sm:absolute bottom-0 xl:left-28 w-5/12">
+        <Image src={botella} alt="Cordillera" layout="responsive" />
+      </div>
     </section>
   );
 };
