fd9fc74e264c6d350cc9994c418c94e8b5491a01
PauRiquelmee <paula.riq.esco@gmail.com>
2022-03-30 18:00:00 -0300

    :lipstick: change image and style

--- components/attributes/Present.jsx
+++ components/attributes/Present.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 
-import cordillera from '../../public/cordillera.jpg';
+import corView from '../../public/corView.jpg';
 
 const Present = () => {
   return (
-    <section className="relative overflow-y-hidden sm:h-96">
-      <Image src={cordillera} alt="Nevados de Chillán" layout="responsive" />
-      <div className="absolute flex flex-col justify-center items-start left-8 top-8 py-1 px-4 sm:py-0 sm:top-3.5 xl:inset-20 xl:left-96 space-y-2 max-w-[17rem] sm:max-w-md bg-white-cor/60 rounded-lg">
+    <section className="relative overflow-y-hidden ">
+      <Image src={corView} alt="Vista Cordillera" layout="responsive" />
+      <div className="absolute flex flex-col justify-center items-start space-y-2 bg-white-cor/60 rounded-lg px-12 inset-96">
         <h1 className="title">Un regalo de la naturaleza</h1>
         <p className="description hidden sm:block">
           Cor es extraída y producida en un paraje virgen e inmaculado posado en
